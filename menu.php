0c73a2f9d631b7108fc9a26a07ec50a69294859a
johnk45 <katanaziro346@gmail.com>
2026-03-23 11:26:01 +0300

    Remove PHP files from repository

--- menu.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo'<a href="/default.asp">Home</a> - 
-<a href="html/default.asp">CSS Tutorial</a> - 
-<a href="/css/default.asp">Javascript Tutorial</a> - 
-<a href="default.asp">PHP Tutorial</a>';
-
-?>
